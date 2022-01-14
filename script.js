3d44cfc46f0e9208bb521974d1672dbd61b1acb9
PJSalter <psalter88@googlemail.com>
2022-01-14 16:22:12 +0000

    modified with an event listener

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 
-const numberArray = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]
+//const numberArray = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8] 
+
+// all of my javaScript will be created in a dom event listener.
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    
+
+})
 
