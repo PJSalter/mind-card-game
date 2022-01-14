15248a345653025fc2fc8d27a498e1223080a6a0
PJSalter <psalter88@googlemail.com>
2022-01-14 17:22:29 +0000

    modified with new function to gaming operation

--- script.js
+++ script.js
@@ -57,7 +57,31 @@ document.addEventListener('DOMContentLoaded', () => {
          }
     ]
 
+    // now I will pick out the element from my html with the class name of mind-game-dimensions with a query selector.
+    const boardStructure = document.querySelector('.mind-game-dimensions');
+
     // Now I will be making the function that will formulate the gaming board.
+    function gameBoard () {
+        //I will now use a for loop which will iterate thru my array of cards.
+        for (let i = 0; i < gameArr.length; i++){
+        //for each card i will make an image element and calling it sea creature.
+        let seaCreature = document.createElement('img')
+        // then I will set an attribute which links it to the most relevent path
+        seaCreature.setAttribute('src', 'Sea-World-Images/blank-jelly-fish.png')
+        // I will give each image/card a data id which will iterate thru 0 - 11.
+        seaCreature.setAttribute('data-id', i)
+        // I will create another add event listener which will listen out to the cards that have been clicked on.
+        // this will provoke a cardTurnover function.
+        //seaCreature.addEventListener('click', cardTurnover)
+
+        // all these cards with different image elements will be placed in the div within the html using the appendChild.
+        boardStructure.appendChild(seaCreature)
+
+        }
+    }
+
+    // now call the function
+    gameBoard()
 
 })
 
