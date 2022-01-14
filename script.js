b1ea374d216b4c2745720d0cb8f2f19d6713c16c
PJSalter <psalter88@googlemail.com>
2022-01-14 16:46:21 +0000

    created an array for the images and updated instructions for next task

--- script.js
+++ script.js
@@ -5,7 +5,59 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    
+    // I will now start off my making an array of cards and then later on I will give them names.
+    const gameArr = [
+         {
+             name: 'dolphin',
+             img: 'Sea-World-Images/dolphin.png'
+         },
+         {
+            name: 'dolphin',
+            img: 'Sea-World-Images/dolphin.png'
+         },
+         {
+             name: 'shark',
+             img: 'Sea-World-Images/shark.png'
+         },
+         {
+            name: 'shark',
+            img: 'Sea-World-Images/shark.png'
+         },
+         {
+            name: 'jellyfish',
+            img: 'Sea-World-Images/jellyFish.png'
+         },
+         {
+            name: 'jellyfish',
+            img: 'Sea-World-Images/jellyFish.png'
+         },
+         {
+             name: 'turtle',
+             img: 'Sea-World-Images/turtle.png'
+         },
+         {
+             name: 'turtle',
+             img: 'Sea-World-Images/turtle.png'
+         },
+         {
+             name: 'octopus',
+             img: 'Sea-World-Images/Octopus.png'
+         },
+         {
+            name: 'octopus',
+            img: 'Sea-World-Images/Octopus.png'
+         },
+         {
+             name: 'seahorse',
+             img: 'Sea-World-Images/seahorse.png'
+         },
+         {
+            name: 'seahorse',
+            img: 'Sea-World-Images/seahorse.png'
+         }
+    ]
+
+    // Now I will be making the function that will formulate the gaming board.
 
 })
 
